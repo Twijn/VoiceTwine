231a9ee3a3961ced13b714e58d52a2006cc2b2af
Tyler Twining <twijn@twijn.net>
2025-06-26 11:16:13 -0500

    fix: Remove sequelize.sync() to be safer in production

--- src/lib/sequelize/models/index.ts
+++ src/lib/sequelize/models/index.ts
@@ -26,9 +26,7 @@ export const initModels = async () => {
     try {
         logger.info(`Attempting to connect to MariaDB @ ${sequelize.config.host}...`);
         await sequelize.authenticate();
-        logger.info("Connected to MariaDB! Synchronizing...");
-        await sequelize.sync({alter: process.env.NODE_ENV === "development"});
-        logger.info("Databases synchronized!");
+        logger.info("Connected to MariaDB!");
     } catch(err) {
         logger.error("Error initializing models: " + err);
         throw new Error(`Failed to connect to database: ${err}`);
